c77df06e643798b839e99805c261e71449815064
Shengela <168460082+Shengelaa@users.noreply.github.com>
2025-07-17 22:10:31 +0400

    rr

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,13 +11,9 @@ let cachedServer: Express;
 async function bootstrap() {
   const expressApp = express();
 
-
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
 
-  app.enableCors({
-    origin: '',
-    credentials: true,
-  });
+  app.enableCors();
 
   app.useGlobalGuards(new LoggingGuard());
 
@@ -33,11 +29,40 @@ async function bootstrap() {
   return expressApp;
 }
 
-export default async function handler(req: Request, res: Response) {
-  if (!cachedServer) {
-    cachedServer = await bootstrap();
-    console.log('✅ Server bootstrapped!');
-  }
+// export default async function handler(req: Request, res: Response) {
+//   if (!cachedServer) {
+//     cachedServer = await bootstrap();
+//     console.log('✅ Server bootstrapped!');
+//   }
 
-  return cachedServer(req, res);
-}
+//   return cachedServer(req, res);
+// }
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { LoggingGuard } from './common/guards/logger.guard';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   });
+
+//   app.useGlobalGuards(new LoggingGuard());
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       transform: true,
+//       forbidNonWhitelisted: true,
+//     }),
+//   );
+
+//   await app.listen(3001);
+//   console.log('🚀 Server is running on http://localhost:3000');
+// }
+
+// bootstrap();
